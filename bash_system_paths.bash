29a7b5c820964899d4fc5e8ae7b408ef45b28ed9
Ron Fritz <fritz@iron.blue>
2025-07-07 20:56:48 -0400

    Check for homebrew before modifying path

--- bash_system_paths.bash
+++ bash_system_paths.bash
@@ -10,12 +10,14 @@ set_os_type
 # MODIFICATIONS BELOW THIS LINE
 
 if [ "$OS_TYPE" = "macos" ]; then
-  prepend_to_path "/opt/homebrew/opt/gnu-tar/libexec/gnubin"
-  prepend_to_path "/opt/homebrew/opt/gnu-sed/libexec/gnubin"
-  prepend_to_path "/opt/homebrew/opt/grep/libexec/gnubin"
-  prepend_to_path "/opt/homebrew/opt/findutils/libexec/gnubin"
-  prepend_to_path "/opt/homebrew/sbin"
-  prepend_to_path "/opt/homebrew/bin"
-  [ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
-  export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+  if [ -x "/opt/homebrew/bin/brew" ]; then
+    prepend_to_path "/opt/homebrew/opt/gnu-tar/libexec/gnubin"
+    prepend_to_path "/opt/homebrew/opt/gnu-sed/libexec/gnubin"
+    prepend_to_path "/opt/homebrew/opt/grep/libexec/gnubin"
+    prepend_to_path "/opt/homebrew/opt/findutils/libexec/gnubin"
+    prepend_to_path "/opt/homebrew/sbin"
+    prepend_to_path "/opt/homebrew/bin"
+    [ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
+    export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+  fi
 fi
